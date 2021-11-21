04192fd688a165cec6f84c15a847be9119223b87
Byron 2016 <bgva2005@yahoo.com>
2021-11-21 17:53:31 -0500

    001_adaptacionpaquete Agregar paquete next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-}
+};
+
+const withTranspileModules = require("next-transpile-modules")([
+  "@super2001/lcomponents",
+]);
+
+module.exports = withTranspileModules({});
