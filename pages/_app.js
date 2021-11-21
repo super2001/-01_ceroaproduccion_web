2acf75724a2436f7d4cb5c11c8f13a2848af35de
Byron 2016 <bgva2005@yahoo.com>
2021-11-21 17:54:38 -0500

    001_adaptacionpaquete Agregar tokens en _App

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@super2001/lcomponents/styles/tokens.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
